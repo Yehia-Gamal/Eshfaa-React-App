b6c4624031e34294d09ec5b3777e820d6f256e69
Yehia-Gamal <yahia.elspaa@gmail.com>
2022-12-27 20:57:28 +0200

    The main Font Family has been changed and some errors that reduce the performance of the site have been fixed, and the video has been deleted and shown through YouTube

--- js/programs_pup.js
+++ js/programs_pup.js
@@ -1,66 +1,61 @@
 function show_pup_1() {
-  document.getElementById("pup_1").classList.add('open')
+  document.getElementById("pup_1").classList.add("open");
 }
 function show_pup_2() {
-  document.getElementById("pup_2").classList.add('open')
+  document.getElementById("pup_2").classList.add("open");
 }
 function show_pup_3() {
-  document.getElementById("pup_3").classList.add('open')
+  document.getElementById("pup_3").classList.add("open");
 }
 function show_pup_4() {
-  document.getElementById("pup_4").classList.add('open')
+  document.getElementById("pup_4").classList.add("open");
 }
 function show_pup_5() {
-  document.getElementById("pup_5").classList.add('open')
+  document.getElementById("pup_5").classList.add("open");
 }
 function show_pup_6() {
-  document.getElementById("pup_6").classList.add('open')
+  document.getElementById("pup_6").classList.add("open");
 }
 function show_pup_7() {
-  document.getElementById("pup_7").classList.add('open')
+  document.getElementById("pup_7").classList.add("open");
 }
 function show_pup_8() {
-  document.getElementById("pup_8").classList.add('open')
+  document.getElementById("pup_8").classList.add("open");
 }
 function show_pup_9() {
-  document.getElementById("pup_9").classList.add('open')
+  document.getElementById("pup_9").classList.add("open");
 }
 function show_pup_10() {
-  document.getElementById("pup_10").classList.add('open')
+  document.getElementById("pup_10").classList.add("open");
 }
-
-
 
 function hide_pup_1() {
-  document.getElementById("pup_1").classList.remove('open')
+  document.getElementById("pup_1").classList.remove("open");
 }
 function hide_pup_2() {
-  document.getElementById("pup_2").classList.remove('open')
+  document.getElementById("pup_2").classList.remove("open");
 }
 function hide_pup_3() {
-  document.getElementById("pup_3").classList.remove('open')
+  document.getElementById("pup_3").classList.remove("open");
 }
 function hide_pup_4() {
-  document.getElementById("pup_4").classList.remove('open')
+  document.getElementById("pup_4").classList.remove("open");
 }
 function hide_pup_5() {
-  document.getElementById("pup_5").classList.remove('open')
+  document.getElementById("pup_5").classList.remove("open");
 }
 function hide_pup_6() {
-  document.getElementById("pup_6").classList.remove('open')
+  document.getElementById("pup_6").classList.remove("open");
 }
 function hide_pup_7() {
-  document.getElementById("pup_7").classList.remove('open')
+  document.getElementById("pup_7").classList.remove("open");
 }
 function hide_pup_8() {
-  document.getElementById("pup_8").classList.remove('open')
+  document.getElementById("pup_8").classList.remove("open");
 }
 function hide_pup_9() {
-  document.getElementById("pup_9").classList.remove('open')
+  document.getElementById("pup_9").classList.remove("open");
 }
 function hide_pup_10() {
-  document.getElementById("pup_10").classList.remove('open')
+  document.getElementById("pup_10").classList.remove("open");
 }
-
-
-
